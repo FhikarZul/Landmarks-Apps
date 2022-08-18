88b7b1e2fd8606a8dd4ad8b04fb7ee2391a98465
Fhikar Zul <Fhikarzul18@gmail.com>
2022-08-18 16:49:10 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lucy on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
